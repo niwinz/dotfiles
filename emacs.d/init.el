11feadb01bbde7445b0e06947b429ab82a9f202e
Andrey Antukh <niwi@niwi.be>
2014-02-06 10:32:33 +0100

    Improved emacs config.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,34 +6,40 @@
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
-(setq column-number-mode t
-      size-indication-mode t)
+;; Use tabs
+(setq-default c-basic-offset 4)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
 
+;; White space mode
+(global-whitespace-mode)
+(setq whitespace-style '(face trailing))
 
-
+;; Look & Feel fixes
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (load-theme 'solarized-dark t)
 (nyan-mode)
-;; (require 'pretty-mode)
-;; (global-pretty-mode 1)
-;; (add-hook 'my-pretty-language-hook 'turn-on-pretty-mode)
-
+(setq column-number-mode t
+      size-indication-mode t)
 
 (defun list-buffers (&optional arg)
   (interactive "P")
   (display-buffer (list-buffers-noselect arg))
   (other-window 1))
 
+;; Idle mode
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 (require 'ido-ubiquitous)
+(ido-vertical-mode)
 (ido-at-point-mode)
+
+;; Projectile
+(projectile-global-mode)
 
 ;; comment and uncomment
 (global-set-key (kbd "C-c C-c") 'comment-region)
 (global-set-key (kbd "C-c C-u") 'uncomment-region)
-(projectile-global-mode)
-(ido-vertical-mode)
