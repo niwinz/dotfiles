0d7b205f8ac61bb49a61604f7e5e99850061605c
Andrey Antukh <niwi@niwi.be>
2014-03-06 11:26:59 +0100

    Add groovy mode.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -88,3 +88,23 @@
     (comment-dwim arg)))
 
 (global-set-key "\M-;" 'comment-dwim-line)
+(global-font-lock-mode 1)
+
+(require 'groovy-mode)
+(require 'groovy-electric)
+
+;; ;; turn on syntax highlighting
+
+;; ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+;; (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+;; (add-to-list 'auto-mode-alist '("\\.groovy" . groovy-mode))
+;; (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;; ;;; make Groovy mode electric by default.
+;; (add-hook 'groovy-mode-hook
+;;           '(lambda ()
+;;              (require 'groovy-electric)
+;;              (groovy-electric-mode)))
+
+
+(add-to-list 'auto-mode-alist '("\\.jinja\\'" . html-mode))
