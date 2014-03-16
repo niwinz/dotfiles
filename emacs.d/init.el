9c33231df6b4756c3dcd1d760612d6689a635f42
Andrey Antukh <niwi@niwi.be>
2014-03-16 16:04:54 +0100

    Change some bindings for emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,24 +7,27 @@
 (package-initialize)
 
 ;; Look & Feel fixes
-
-(setq-default c-basic-offset 4)
+(setq-default c-basic-offset 2)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(setq-default visible-cursor nil)
 
-(global-font-lock-mode 1)
 (global-whitespace-mode)
 (setq whitespace-style '(face trailing))
 (setq dabbrev-case-fold-search nil)
+;; (global-font-lock-mode 1)
 
+(blink-cursor-mode 0)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(load-theme 'solarized-light t)
+(load-theme 'misterioso t)
 (nyan-mode)
 (setq column-number-mode t
       size-indication-mode t)
 
+
+;; Backup temporal directories
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 
@@ -32,12 +35,6 @@
       `((".*" ,temporary-file-directory t)))
 
 
-(defun list-buffers (&optional arg)
-  (interactive "P")
-  (display-buffer (list-buffers-noselect arg))
-  (other-window 1))
-
-(add-to-list 'auto-mode-alist '("/requirements\\.txt\\'" . conf-mode))
 
 ;; Ido mode
 (setq ido-enable-flex-matching t)
@@ -88,40 +85,32 @@
       (kill-new (buffer-substring beg end))))
   (beginning-of-line 2))
 
-(global-set-key (kbd "<f9>") 'quick-copy-line)
-(global-set-key (kbd "<f10>") 'kill-whole-line)
+;; (global-set-key (kbd "<f9>") 'quick-copy-line)
+;; (global-set-key (kbd "<f10>") 'kill-whole-line)
 
 (global-unset-key (kbd "C-z"))
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-block)
 (global-set-key (kbd "C-z C-z") 'comment-or-uncomment-block)
 
-;; original idea from
-;; http://www.opensubscriber.com/message/emacs-devel@gnu.org/10971693.html
-;; (defun comment-dwim-line (&optional arg)
-;;   "Replacement for the comment-dwim command.
-;;   If no region is selected and current line is not blank and we are not at the end of the line,
-;;   then comment current line.
-;;   Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
-;;   (interactive "*P")
-;;   (comment-normalize-vars)
-;;   (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
-;;       (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-;;     (comment-dwim arg)))
+(global-unset-key (kbd "C-k"))
+(global-unset-key (kbd "C-l"))
 
-;;(global-set-key "\M-;" 'comment-dwim-line)
+(global-set-key (kbd "C-k") 'kill-whole-line)
+(global-set-key (kbd "C-l") 'quick-copy-line)
+(global-set-key (kbd "C-c C-k") 'kill-line)
+
 
 ;; TODO: should be moved on a hook
 (require 'groovy-mode)
 (require 'groovy-electric)
 
 ;; Hooks
-
 (add-hook 'html-mode-hook
           (lambda ()
             ;; Default indentation is usually 2 spaces, changing to 4.
             (set (make-local-variable 'sgml-basic-offset) 4)))
 
 (add-to-list 'auto-mode-alist '("\\.jinja\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("/requirements\\.txt\\'" . conf-mode))
 
 ;; Abbrevs
 (setq-default abbrev-mode t)
