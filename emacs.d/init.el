86e35238e5913de0aeff932a382c6ae7ed7efc8c
Andrey Antukh <niwi@niwi.be>
2015-02-11 14:37:11 +0100

    Add repos to emacs config.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,16 @@
 ;; Package management
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives
+;; 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+
 (package-initialize)
 
 ;; Look & Feel fixes
