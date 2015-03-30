e9b213a20788e92a3e6ce6ad5cd4b7bf49834575
Andrey Antukh <niwi@niwi.be>
2015-03-30 14:30:26 +0200

    Add web mode.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -55,6 +55,17 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward)
 
+;; Web Mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(setq web-mode-engines-alist
+      '(("django"    . "\\.html\\'")))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+;; (setq web-mode-enable-current-column-highlight t)
+;; (setq web-mode-enable-current-element-highlight t)
+
 ;; Nyan Cat
 (require 'nyan-mode)
 (nyan-mode)
